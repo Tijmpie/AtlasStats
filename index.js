aff3e006bac625ea28169afb40ead3f2b83ddca2
Timjime <wolters.itijm@gmail.com>
2021-01-20 15:47:42 +0100

    Persistant data
    Atlas data now shows across sessions

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-var verdict = 0
-var ban = 0
+import PVObject from "PersistentData";
+
+var data = new PVObject("AtlasStats", {
+    verdict: 0,
+    ban: 0
+});
 
 register("chat", function(message, event){
-    verdict++;
-}).setCriteria("Atlas verdict submitted! Thank you :)");
+    data.verdict++;
+}).setCriteria("&r&aAtlas verdict submitted! Thank you :)&r");
 
 register("chat", function(message, event){
-    ban++;
-}).setCriteria("+2,000 Hypixel Experience (Positive Atlas Verdict)");
+    data.ban++;
+}).setCriteria("&8+&r&32,000 &r&7Hypixel Experience (Positive Atlas Verdict)&r");
 
 register("renderOverlay", myRenderOverlay);
 function myRenderOverlay() {
-    var title = new Text("Session statistics for Hypixel Atlas", 10, 10).setColor(Renderer.GOLD);
-    var verdictRender = new Text("Verdicts: " + verdict, 10, 20);
-    var banRender = new Text("Bans: " + ban, 10, 30);
+    var title = new Text("Statistics for Hypixel Atlas", 10, 10).setColor(Renderer.GOLD);
+    var verdictRender = new Text("Verdicts: " + data.verdict, 10, 20);
+    var banRender = new Text("Bans: " + data.ban, 10, 30);
 
     title.draw();
     verdictRender.draw();
