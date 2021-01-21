62e796e060be9e0e7ccd7eb80047f58bccfe2ad3
Timjime <wolters.itijm@gmail.com>
2021-01-21 12:16:15 +0100

    Extra settings

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
+// Imports
 import PVObject from "PersistentData";
 import { Setting, SettingsObject } from "SettingsManager/SettingsManager";
 
+
+// Settings menu
 const settings = new SettingsObject("AtlasStats", [{
         name: "Display Settings",
 		settings:[
             new Setting.Toggle("Enable hud", true),
             new Setting.Toggle("Ban rate", true),
-            new Setting.Slider("Ban rate decimals", 1, 1, 5, 0)
+            new Setting.Slider("Ban rate decimals", 1, 1, 5, 0),
+            new Setting.Slider("Text location X", 10, 0, Renderer.screen.getWidth(), 0),
+            new Setting.Slider("Text location Y", 10, 0, Renderer.screen.getHeight(), 0)
 		],
 	},
-]).setCommand('ats').setSize(250, 60);
+]).setCommand('ats').setSize(250, 110);
 Setting.register(settings);
 
+
+// Persistant data for statistics
 var data = new PVObject("AtlasStats", {
     verdict: 0,
     ban: 0
 });
 
+
+// Keybinds
+const TPsus = new KeyBind("Teleport to suspect", Keyboard.KEY_NONE, "AtlasStats")
+
+register("tick", function(){
+    if(TPsus.isPressed()){
+        ChatLib.command("tp suspect")
+    }
+});
+
+
+// Registration for verdicts and bans
 register("chat", function(message, event){
     data.verdict++;
 }).setCriteria("&r&aAtlas verdict submitted! Thank you :)&r");
@@ -25,20 +44,30 @@ register("chat", function(message, event){
     data.ban++;
 }).setCriteria("&8+&r&32,000 &r&7Hypixel Experience (Positive Atlas Verdict)&r");
 
+
+// Rendering of the text
 register("renderOverlay", myRenderOverlay);
 function myRenderOverlay() {
     if (settings.getSetting("Display Settings", "Enable hud")) {
-        var title = new Text("Statistics for Hypixel Atlas", 10, 10).setColor(Renderer.GOLD);
-        var verdictRender = new Text("Verdicts: " + data.verdict, 10, 20);
-        var banRender = new Text("Bans: " + data.ban, 10, 30);
+        var x = Number(settings.getSetting("Display Settings", "Text location X"));
+        var y = Number(settings.getSetting("Display Settings", "Text location Y"));
+        var banRate = ((data.ban / data.verdict) * 100).toFixed(settings.getSetting("Display Settings", "Ban rate decimals"))
+
+        var title = new Text("Statistics for Hypixel Atlas", x, y).setColor(Renderer.GOLD);
+        var verdictRender = new Text("Verdicts: " + data.verdict, x, y + 10);
+        var banRender = new Text("Bans: " + data.ban, x, y + 20);
 
         title.draw();
         verdictRender.draw();
         banRender.draw();
 
+
+        // Takes the percentage of bans
         if (settings.getSetting("Display Settings", "Ban rate")) {
-            var banRate = new Text("Ban rate: " + ((data.ban / data.verdict) * 100).toFixed(settings.getSetting("Display Settings", "Ban rate decimals")) + "%", 10, 40)
-            banRate.draw();
+            var banPercent = new Text(
+                "Ban rate: " + banRate + "%", x, y + 30);
+
+            banPercent.draw();
         }
     }
 }
